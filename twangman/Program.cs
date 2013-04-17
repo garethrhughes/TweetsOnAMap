44a6d6fd8a3462dff90962d7d8aa8b0d5eb562a3
Daphne Chong <daphne@daphne>
2013-04-17 22:40:37 +1000

    Working project reading from an open tweet stream using tweetsharp (patched)

---
+++ twangman/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using TweetSharp;
+
+namespace twitter_hangman
+{
+    class Program
+    {
+        private const string _consumerKey = "";
+        private static string _consumerSecret = "";
+        private static string _accessToken = "";
+        private static string _accessTokenSecret = "";
+
+        static void Main(string[] args)
+        {
+            var service = new TwitterService(_consumerKey, _consumerSecret);
+            service.AuthenticateWith(_accessToken, _accessTokenSecret);
+
+           
+
+            
+            IAsyncResult result = service.StreamUser(
+                (tweets, response) =>
+                    {
+                        //if (response.StatusCode == HttpStatusCode.OK)
+                        {
+                            Console.WriteLine(response);
+                            Console.WriteLine(tweets.RawSource);
+
+                            //foreach (var tweet in tweets)
+                            //{
+                            //    Console.WriteLine("{0} said '{1}'", tweet.User.ScreenName, tweet.Text);
+                            //}
+                        }
+                    });
+
+            Console.WriteLine(result.AsyncState);
+
+            //var tweets = service.ListTweetsOnHomeTimeline(new ListTweetsOnHomeTimelineOptions());
+   
+            //foreach (var tweet in tweets)
+            //{
+            //    Console.WriteLine("{0} says '{1}'", tweet.User.ScreenName, tweet.Text);
+            //}
+            
+            Console.ReadLine();
+            service.CancelStreaming();
+        }
+    }
+}
+ 
