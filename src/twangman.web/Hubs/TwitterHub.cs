0fc2abf7ca4daf71ad92cb0095ee7ba8d94b4e71
Gareth Hughes <gareth@grhughes.com>
2013-04-19 10:32:38 +1000

    Client count reliability

--- src/twangman.web/Hubs/TwitterHub.cs
+++ src/twangman.web/Hubs/TwitterHub.cs
@@ -1,6 +1,8 @@
 ﻿namespace twangman.web.Hubs
 {
   using System;
+  using System.Collections.Generic;
+  using System.Linq;
   using System.Threading.Tasks;
   using Microsoft.AspNet.SignalR;
   using Microsoft.AspNet.SignalR.Hubs;
@@ -44,14 +46,15 @@
     private static readonly Lazy<TwitterTicker> _instance = new Lazy<TwitterTicker>(
       () => new TwitterTicker(GlobalHost.ConnectionManager.GetHubContext<TwitterHub>().Clients));
 
-    private int _userCount;
-
+    
     private TwitterTicker(IHubConnectionContext clients)
     {
       Clients = clients;
     }
 
     protected IHubConnectionContext Clients { get; set; }
+
+    protected IList<string> ClientIDs { get; set; }
 
     public static TwitterTicker Instance
     {
@@ -60,19 +63,23 @@
 
     public int UserCount
     {
-      get { return _userCount; }
+      get { return ClientIDs.Count; }
     }
 
-    public void DecreaseUsers()
+    public void DecreaseUsers(string id)
     {
-      _userCount -= 1;
-      Clients.All.updateUserCount(_userCount);
+      if(!ClientIDs.Any(x => x == id))
+        ClientIDs.Add(id);
+
+      Clients.All.updateUserCount(UserCount);
     }
 
-    public void IncreaseUsers()
+    public void IncreaseUsers(string id)
     {
-      _userCount += 1;
-      Clients.All.updateUserCount(_userCount);
+      if (ClientIDs.Any(x => x == id))
+        ClientIDs.Remove(id);
+
+      Clients.All.updateUserCount(UserCount);
     }
 
       public void SendPostcode(int postcode, int size, double rating, double latitude, double longitude, string text, string screenName, string profileImageUrl)
